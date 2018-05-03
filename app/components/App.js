c4f497401a2b56de7ccf191e30278a2270fe0c8b
Hadi Yousufi <reach.hadiy@gmail.com>
2018-05-03 15:34:21 -0500

    renders popular comp with proper onClick highlighting

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,8 @@ const Popular = require('./Popular');
 
 class App extends React.Component {
   render() {
+   
+
     return (
       <div className='container'>
         <Popular />
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,40 @@
 const React = require('react');
 
-class Popular extends React.Component{
-    render() {
-        return (
-            <div>Sniffy</div>
-        )
-    }
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+  }
+
+  updateLanguage(lang) {
+    this.setState({
+      selectedLanguage: lang
+    });
+  }
+
+  render() {
+    let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <ul className="languages">
+        {languages.map(lang => (
+          <li
+            style={
+              lang === this.state.selectedLanguage ? { color: '#d0021b' } : null
+            }
+            onClick={() => {
+              this.updateLanguage(lang);
+            }}
+            key={lang}
+          >
+            {lang}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 module.exports = Popular;
