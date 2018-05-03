e1c27f78d5b35495d30c202ab8e222af54207722
Hadi Yousufi <reach.hadiy@gmail.com>
2018-05-02 22:53:13 -0500

    adds app component back

--- app/index.js
+++ app/index.js
@@ -3,28 +3,16 @@ const ReactDOM = require('react-dom');
 require('./index.css');
 const PropTypes = require('prop-types');
 
-class Badge extends React.Component {
+class App extends React.Component {
+
   render() {
+
     return (
       <div>
-        <img src={this.props.img} />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
+        <p>Hello World!!!</p>
       </div>
-    );
+    )
   }
 }
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired
-};
 
-ReactDOM.render(
-  <Badge
-    name="Tyler McGinnis"
-    username="tylermcginnis"
-    img="https://avatars0.githubusercontent.com/u/2933430?v=3&s=460"
-  />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
