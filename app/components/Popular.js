364966c8c157c4cf56be0f6a48635701c49ec89e
Hadi Yousufi <reach.hadiy@gmail.com>
2018-05-03 20:13:17 -0500

    re adds poular comp

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,39 +1,33 @@
 const React = require('react');
 
+
 class Popular extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       selectedLanguage: 'All'
-    };
+    }
   }
-
+  
   updateLanguage(lang) {
     this.setState({
-      selectedLanguage: lang
-    });
-  }
+    selectedLanguage: lang
+  }) 
+}
 
   render() {
     let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
     return (
-      <ul className="languages">
-        {languages.map(lang => (
-          <li
-            style={
-              lang === this.state.selectedLanguage ? { color: '#d0021b' } : null
-            }
-            onClick={() => {
-              this.updateLanguage(lang);
-            }}
+      <ul className='languages'>
+        {languages.map(lang => {
+          return <li
+            style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null}  
             key={lang}
-          >
-            {lang}
-          </li>
-        ))}
+            onClick={() => this.updateLanguage(lang)}
+          >{lang}</li>
+        })}
       </ul>
-    );
+    )
   }
 }
 
